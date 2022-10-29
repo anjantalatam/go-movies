ecb59c7478037979699d01919a1eed04de5d3a00
Anjan Talatam <tspa2117@gmail.com>
2022-10-29 18:06:22 +0530

    31 - More about routing and a bit about the React lifecycle

--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+interface IMovie {
+  id: string;
+  title: string;
+  runTime: Number;
+}
 
 function Movies() {
+  const [movies, setMovies] = useState<IMovie[]>([]);
+
+  useEffect(() => {
+    setMovies([
+      { id: "1", title: "The Shawshank Redemption", runTime: 142 },
+      { id: "2", title: "The Godfather", runTime: 175 },
+      { id: "3", title: "The Dark Knight", runTime: 153 },
+    ]);
+  }, []);
+
   return (
     <>
       <h2>Choose a Movie</h2>
+      <ul>
+        {movies.map((m) => (
+          <li key={m.id}>{m.title}</li>
+        ))}
+      </ul>
     </>
   );
 }
